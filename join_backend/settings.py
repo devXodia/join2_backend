4abd910a42f15cb321ac302c3a3b2104c9ee11d6
devXodia <youstylez@gmail.com>
2024-03-25 18:51:07 +0100

    added venv

--- join_backend/settings.py
+++ join_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-+k8ery6)sk1t3-_i4=o66$rzbjz^*7zds=e&!ypfbl5^#$5@qx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['devXodia.pythonanywhere.com']
+ALLOWED_HOSTS = ['devXodia.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
