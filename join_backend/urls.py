c740476d061c8e8f084faea738045190f8283d41
devXodia <youstylez@gmail.com>
2024-03-31 11:58:42 +0200

    changed urls

--- join_backend/urls.py
+++ join_backend/urls.py
@@ -25,8 +25,7 @@ router = routers.DefaultRouter()
 router.register(r'tasks', TaskViewSet)
 router.register(r'contacts', ContactsViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
     path('', include(router.urls)),
     path("admin/", admin.site.urls),
